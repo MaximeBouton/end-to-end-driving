f8c6059f9f952ad7ba794c962951df2ee62064c4
heyse <heyse@stanford.edu>
2016-10-27 12:53:00 -0700

    ternary classification (should check on thresholds) + basic stats

--- src/baselineTernaryClassification.py
+++ src/baselineTernaryClassification.py
@@ -10,11 +10,15 @@ fname = '../../data/baselineTernaryClassification.csv'
 fw = open(fname,'w')
 fw.write('id,classification')
 
-
 # steering angle thresholds for left / straight / right
 thetaL = .05    # don't know any more which values make sense
 thetaR = -.05   # script runs in < 1 sec so we can adjust easily
 classification = 0
+
+# some stats
+totalL = 0
+totalC = 0
+totalR = 0
 
 # loop through all samples from input and write classification to output
 line = fr.readline()
@@ -24,13 +28,25 @@ while line != '':
     steeringAngle = float(line_i[3])
     if steeringAngle >= thetaL:
         classification = 1
+        totalL += 1
     elif steeringAngle <= thetaR:
         classification = -1
+        totalR += 1
     else:
         classification = 0
+        totalC += 1
     fw.write('\n' + id + ',' + str(classification))
     line = fr.readline()
 
 # finish
 fr.close()
 fw.close()
+
+# publish stats
+totalCount = totalL+totalC+totalR
+print('total sample # : {}'.format(totalCount))
+print(' >>       left : {} [{}%]'.format(totalL,round(float(totalL)/totalCount*100,1)))
+print(' >>   straight : {} [{}%]'.format(totalC,round(float(totalC)/totalCount*100,1)))
+print(' >>      right : {} [{}%]'.format(totalR,round(float(totalR)/totalCount*100,1)))
+
+
