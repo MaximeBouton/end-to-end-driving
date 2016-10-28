3acaf6524bb74010bbad34b37988da6c39851140
MaximeBouton <maxim.bouton@gmail.com>
2016-10-27 21:06:30 -0700

    finish script to extract features from imgs

--- src/image_to_features.py
+++ src/image_to_features.py
@@ -3,17 +3,33 @@ import numpy as np
 
 path = '../../data/centerCamera/'
 
-# size of the training set
-m = 100
+
+# Remove  Outlayers
+
+ranges = [(1,1590),(1844,4965),(5180,8878),(9985,11380),(11740,13340),(13710,14060),(14680,15120)]
+
+bigRange = []
+
+for r in ranges:
+    bigRange+= range(r[0],r[1]+1)
+    
+
+#size of the training set
+
+m = len(bigRange)
+print m
 w,h = 640,480
 red = 20
 w_,h_ = w/red,h/red
 
 Phi = np.zeros((m,w_,h_,3))
 
+
 print 'Start processing images'
-for i in range(m):
-    imgId = '%05d' %(i+1)
+for (i,j) in enumerate(bigRange):
+    if i%100==0:
+        print 'Processing image %i' %i
+    imgId = '%05d' %(j+1)
     imgName = path+imgId+'.jpeg'
     image = cv2.imread(imgName)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
