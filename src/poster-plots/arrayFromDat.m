c9c256da9e5e9e4085d60995522cbb9f622162c3
Jan Felix Heyse <heyse@Jans-MacBook-Pro.local>
2016-12-05 00:33:59 -0800

    poster-folder with plots

--- src/poster-plots/arrayFromDat.m
+++
@@ -1,7 +0,0 @@
-function array = arrayFromDat(fname)
-fid = fopen(fname);
-L = str2double(fid.fgetl());
-array = zeros(L,1);
-for l = 1:L
-    array(l) = str2double(fid.fgetl());
-end
--- src/poster-plots/npy2cvs.py
+++ src/poster-plots/npy2cvs.py
@@ -2,10 +2,13 @@ import numpy as np
 
 
 #path  = './classification/'
-path  = './prelu_3_3_3_60/'
+# path  = './prelu_3_3_3_60/'
+path  = './prelu_3_2_3_60/'
 
 #filename = 'train_loss'
-filename = 'train45'
+# filename = 'train45'
+# filename = 'train_loss_prelu_3_2_3_6'
+filename = 'train_loss_prelu_3_2_3_6_cntd'
 
 readname  = path + filename + '.npy'
 writename = path + filename + '.dat'
@@ -18,7 +21,9 @@ fid.close()
 
 
 #filename = 'val_loss'
-filename = 'val45'
+# filename = 'val45'
+# filename = 'val_loss_prelu_3_2_3_6'
+filename = 'val_loss_prelu_3_2_3_6_cntd'
 
 readname  = path + filename + '.npy'
 writename = path + filename + '.dat'
@@ -31,7 +36,9 @@ fid.close()
 
 
 #filename = 'test_loss'
-filename = 'test45'
+# filename = 'test45'
+# filename = 'test_loss_prelu_3_2_3_6'
+filename = 'test_loss_prelu_3_2_3_6_cntd'
 
 readname  = path + filename + '.npy'
 writename = path + filename + '.dat'
@@ -41,5 +48,3 @@ fid = open(writename, 'w')
 for i in range(L):
     fid.write(str(A[i]) + '\n')
 fid.close()
-
-
